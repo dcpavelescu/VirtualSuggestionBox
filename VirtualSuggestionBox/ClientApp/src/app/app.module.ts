fc02b7e76d47d0fb5b59335ea31bbf5360e7c5d0
Daniel Pavelescu <daniel.pavelescu@digital-moves.eu>
2018-07-11 08:42:20 +0300

    change

--- VirtualSuggestionBox/ClientApp/src/app/app.module.ts
+++ VirtualSuggestionBox/ClientApp/src/app/app.module.ts
@@ -1,22 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
-import { BarRatingModule } from "ngx-bar-rating";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    HttpClientModule,
-    FormsModule,
-    BarRatingModule
   ],
   bootstrap: [AppComponent]
 })
