8ea9964fe3bbe85592bf3930389030a104a8d2ae
Rares Ont <rares.ont@gmail.com>
2018-07-11 19:30:00 +0300

    Suggestion class

---
+++ VirtualSuggestionBoxApi/Models/Suggestion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VirtualSuggestionBoxApi.Models
+{
+    public class Suggestion
+    {
+        private String SuggestionId { get; set; }
+        private String Improvement { get; set; }
+        private String Solution { get; set; }
+        private String EmployeeId { get; set; }
+
+        private DateTime Date { get; set; }
+        private List<int> Ratings { get; set; }
+        private List<string> Category { get; set; }
+           
+        public Suggestion()
+        {
+            Ratings = new List<int>();
+            Category = new List<string>();
+
+        }
+        
+    }
+}
