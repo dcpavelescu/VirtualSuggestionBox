09d07bfe44c89c83c28808d94bf58f04522e58d4
Catalin Tudoran <tudorancatalin96@gmail.com>
2018-07-11 15:37:03 +0300

    Created header, footer, home view(a try) and add new suggestion view.

--- VirtualSuggestionBox/ClientApp/src/app/app.module.ts
+++ VirtualSuggestionBox/ClientApp/src/app/app.module.ts
@@ -1,14 +1,37 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AddNewSuggestionComponent } from './addNewSuggestion/add-new-suggestion.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { HomeComponent } from './home/home.component';
 
+const appRoutes: Routes = [
+  { path: 'add-new-suggestion', component: AddNewSuggestionComponent },
+  { path: 'home', component: HomeComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AddNewSuggestionComponent,
+    HomeComponent
   ],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+  ],
+
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
