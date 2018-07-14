a76a2e7e082e5b64047907e21aba3dca5e3343db
Daniel Pavelescu <daniel.pavelescu@digital-moves.eu>
2018-07-14 22:51:40 +0300

    unit test example

--- VisualSuggestionBoxTest/SuggestionTests.cs
+++ VisualSuggestionBoxTest/SuggestionTests.cs
@@ -1,11 +1,29 @@
 using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+using VirtualSuggestionBoxApi.Storages;
 
 namespace VisualSuggestionBoxTest
 {
     [TestClass]
     public class SuggestionTests
     {
+        public SuggestionTests()
+        {
+            
+
+        }
+
+        [TestMethod]
+        public void TestSuggestionSaveInMemory()
+        {
+            SuggestionInMemory suggestionInMemory = new SuggestionInMemory();
+            suggestionInMemory.dictionary.Add("id1", new VirtualSuggestionBoxApi.Models.Suggestion("dasdsad", "dssdsa", "employee1"));
+            suggestionInMemory.dictionary.Add("id2", new VirtualSuggestionBoxApi.Models.Suggestion("ggdgd", "fgfd", "employee1"));
+            suggestionInMemory.dictionary.Add("id3", new VirtualSuggestionBoxApi.Models.Suggestion("gfdgd", "gfgd", "employee1"));
+
+            Assert.AreEqual(suggestionInMemory.dictionary.Count, 3);
+        }
+
     }
 }
