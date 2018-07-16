57cd02456a35afbf72cdb7f7467e096336d52632
Rares Ont <rares.ont@gmail.com>
2018-07-16 22:14:10 +0300

    suggestion controller 0.5

--- VirtualSuggestionBoxApi/Controllers/SuggestionController.cs
+++ VirtualSuggestionBoxApi/Controllers/SuggestionController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
 using VirtualSuggestionBoxApi.Models;
 
 namespace VirtualSuggestionBoxApi.Controllers
@@ -12,20 +13,72 @@ namespace VirtualSuggestionBoxApi.Controllers
     [ApiController]
     public class SuggestionController : ControllerBase
     {
-     //   [Route("api/[controller]")]
-        
-      //  public IActionResult Index()
-      //  {
-            
-    //    //    return ViewComponent();
-   //     }
-    //    public IActionResult Suggestion()
-    //    {
-       //     Suggestion s1;
 
-                              // return View();
-    //    }
-        
+        MongoDBStorage _db;
+        public SuggestionController()
+        {
+            _db = new MongoDBStorage();
+        }
+
+        //  GET: api/Suggestion
+        [HttpGet]
+        public IEnumerable<Suggestion> Get()
+        {
+
+            return _db.GetSuggestions();
+        }
+
+
+        // GET: api/Suggestion/5
+        [HttpGet("{id}", Name = "Get")]
+        public ObjectResult Get(ObjectId id)
+        {
+            var suggestion = _db.GetSuggestion(id);
+            if (suggestion == null)
+            {
+                return NotFound("not found!");
+            }
+            return new ObjectResult(suggestion);
+        }
+
+        // POST: api/Suggestion
+        [HttpPost]
+        public IActionResult Post([FromBody] Suggestion a)
+        {
+            _db.CreateSuggestion(a);
+            return new OkObjectResult(a);
+
+        }
+
+        // PUT: api/Suggestion/5
+        [HttpPut("{id}")]
+        public IActionResult Put(ObjectId id, [FromBody] Suggestion a)
+        {
+            var recId = id;
+            var suggestion = _db.GetSuggestion(recId);
+            if (suggestion == null)
+            {
+                return NotFound();
+            }
+
+            _db.UpdateSuggestion(recId, a);
+            return new OkResult();
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(ObjectId id)
+        {
+            var suggestion = _db.GetSuggestion(id);
+            if (suggestion == null)
+            {
+                return NotFound();
+            }
+
+            _db.RemoveSuggestion(suggestion.SuggestionID);
+            return new OkResult();
+
+        }
 
     }
 }
